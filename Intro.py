05fc805e651b8b5cdd2a95cbf4e139b4c492794a
Ananya419 <shahiananya381@gmail.com>
2026-01-01 23:05:37 +0530

    My First LeetCode Solution

---
+++ Intro.py
@@ -0,0 +1,7 @@
+# For creating Virtual Environment we use this command : python -m venv venv
+# To activate the virtual environment use this command : .\venv\Scripts\activate
+#  To run A python Program use this command : python filename.py
+# Bugs in a program is known as Error or Mistakes.
+# There are mainly two types of errors in Python Programming :
+# 1. Syntax Error : These errors occur when the code is not written in proper format
+# 2. Exception Error : These errors occur when the code is syntactically correct but there is some logical error in the code.
