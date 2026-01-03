cf20d6a8bd19777b889e5b4a53f50a917adaea90
Ananya419 <shahiananya381@gmail.com>
2026-01-03 22:01:21 +0530

    My Second Solution

--- Three_Sum.py
+++ Three_Sum.py
@@ -1,33 +1,40 @@
-class Solution:
-    def threeSum(self, nums: list[int]) -> list[list[int]]:
+class Solution(object):
+    def threeSum(self, nums):
+        #here i have sort the array
         nums.sort()
+        # Here i have taken the length of the array
         n = len(nums)
+        # here i have taken an empty array to store answers
         answer = []
 
-        for i in range(n):
-            if nums[i] > 0:
-                break
-            elif i > 0 and nums[i] == nums[i-1]:
-                continue
+        # here i have applied for loop for assigning the values of i and the i is the fixed pointer 
+        for i in range(n-2):
+            #here i have taken two pointer which is l and r 
+            l,r = i+1 , n-1
 
-            lo,hi = i+1, n-1
-            while lo < hi:
-                sum = nums[i] + nums[lo] + nums[hi]
+            # here i have applied the while loop for checking the conditions
+            
+            while l < r:
+                sum = nums[i] + nums[l] + nums[r]
+                # when the value of sum is equal to zero then we will append the answer
                 if sum == 0:
-                    answer.append([nums[i],nums[lo],nums[hi]])
-                    lo,hi = lo+1 , hi-1
-                    while lo < hi and nums[lo] == nums[lo-1]:
-                        lo += 1
-                    while lo < hi and nums[hi] == nums[hi+1]:
-                        hi -= 1
+                    # here we have used this for reduce the duplicate values
+                    if [nums[i], nums[l], nums[r]] in answer:
+                        l+=1
+                        r-=1
+                        continue
+                    answer.append([nums[i], nums[l], nums[r]])
+                    l+=1
+                    r-=1
+                # when the value ofsum is less than zero then we will increment the left pointer 
                 elif sum < 0:
-                    lo += 1
+                    l += 1
+                # when the value ofsum is greater than zero then we will decrement the right pointer
                 else:
-                    hi -= 1
-        return answer                   
-
+                    r -= 1
+        return answer
         
-# Example usage:
-solution = Solution()
-nums = [-1, 0, 1, 2, -1, -4]
-print(solution.threeSum(nums))
+
+obj = Solution()
+nums = obj.threeSum([-1, 0, 1, 2, -1, -4])
+print(nums)
