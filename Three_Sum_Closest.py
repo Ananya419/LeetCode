626e6b7b8f592e95acb7ff60c063765512ee8e62
Ananya419 <shahiananya381@gmail.com>
2026-01-04 22:19:35 +0530

    MY Thirrd Solution

---
+++ Three_Sum_Closest.py
@@ -0,0 +1,33 @@
+class Solution:
+    def threeSumClosest(self, nums, target):
+        nums.sort()
+        n = len(nums)
+        closest = nums[0] + nums[1] + nums[2]  
+
+        for i in range(n - 2):
+            left, right = i + 1, n - 1
+            while left < right:
+                current = nums[i] + nums[left] + nums[right]
+
+                
+                if abs(current - target) < abs(closest - target):
+                    closest = current
+
+                
+                if current < target:
+                    left += 1
+                elif current > target:
+                    right -= 1
+                else:
+                    return current  
+
+        return closest
+
+
+
+ 
+
+nums = [-1, 2, 1, -4]
+target = 1
+obj = Solution()
+print(obj.threeSumClosest(nums, target))  
