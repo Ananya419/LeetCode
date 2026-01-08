7838a71790860dfa02cb387de00e348d32b93f77
Ananya419 <shahiananya381@gmail.com>
2026-01-08 23:18:05 +0530

    My Seventh LeetCode Solution

---
+++ Move_Zeros.py
@@ -0,0 +1,30 @@
+class Solution:
+    def moveZeroes(self, nums):
+        # pointer for the position of the next non-zero element
+        j = 0
+        # for loop to iterate through the list
+        for i in range(len(nums)):
+            # check whether the current element is equal to zero or not
+            if nums[i] != 0:
+                # Move the non-zero element to the j-th position
+                nums[j] = nums[i]
+                # Increment the position for the next non-zero element
+                j += 1
+
+        # it checks whether j is less than the length of the nums list
+        while j < len(nums):
+            # fill the remaining positions with zeros
+            nums[j] = 0
+            # Increment the position for the next zero element
+            j += 1
+
+
+# Example usage:
+# it defines a list of numbers
+nums = [0, 1, 0, 3, 12]
+# move all zeros to the end
+nums = [0, 1, 0, 3, 12]
+# call the moveZeroes method
+Solution().moveZeroes(nums)
+# print the new list
+print(nums)
