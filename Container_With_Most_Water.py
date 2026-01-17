8de3de5ffd67a0751b2afe491a5cc9ef9b70bfd4
Ananya419 <shahiananya381@gmail.com>
2026-01-17 23:35:18 +0530

    My Updated LeetCode Solution

--- Container_With_Most_Water.py
+++ Container_With_Most_Water.py
@@ -1,29 +1,42 @@
 class Solution:
+    # function to calculate maximum area of water container
     def maxArea(self, nums):
+        # initialize variables
         max_height = 0                  
         max_a = 0                  
         l = 0                 
         r = len(nums) - 1 
 
+        # while left pointer is less than right pointer
         while l < r:
+            # calculate width
             w = r - l
+            # calculate height
             height = min(nums[l], nums[r])
+            # calculate area
             area = w * height
 
+            # update maximum area if current area is greater
             if area > max_a:
                 max_a = area
 
+
+            # update maximum height if current height is greater
             if height > max_height:
                 max_height = height
 
+            # move the pointer pointing to the shorter line
             if nums[l] < nums[r]:
+                # move left pointer to the right
                 l += 1
+            #  else if nums[l] > nums[r]:
             else:
+                # move right pointer to the left
                 r -= 1
 
+        # return the maximum area found
         return max_a
-
-obj = Solution()
-
+# Example usage
+solution = Solution()
 heights = [1,8,6,2,5,4,8,3,7]
-print(obj.maxArea(heights))
+print(solution.maxArea(heights))  # Output: 49
