105fee3158e079cd61f33a88501452039066bef1
Ananya419 <shahiananya381@gmail.com>
2026-02-02 21:47:27 +0530

    LeetCode Solution No. Thirty-one

---
+++ Search_In_Rotated_Sorted_Array.py
@@ -0,0 +1,34 @@
+class Solution:
+    def search(self, nums, target):
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            # Left half is sorted
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                # Right half is sorted
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+        return -1
+    
+# Example Usage:
+    
+nums = [4,5,6,7,0,1,2]
+target = 0
+
+solution = Solution()
+result = solution.search(nums, target)
+print("Index of target:", result)
+
