74e89b57d5e5a6438e2ba88e19c7bc16346104f3
Ananya419 <shahiananya381@gmail.com>
2026-02-08 23:52:25 +0530

    LeetCode Daily Challenges solution_08

---
+++ Daily_Challenges_LC/daily_challenges_08.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def getHeight(node):
+            if not node:
+                return 0
+            
+            left_height = getHeight(node.left)
+            if left_height == -1:
+                return -1
+            
+            right_height = getHeight(node.right)
+            if right_height == -1:
+                return -1
+            
+            if abs(left_height - right_height) > 1:
+                return -1
+            
+            return max(left_height, right_height) + 1
+        
+        return getHeight(root) != -1
+
+# Example Usage
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+
+sol = Solution()
+print(sol.isBalanced(root))  # Output: True
