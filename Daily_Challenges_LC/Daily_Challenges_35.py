64afad3ca20a07f0c2f20c51e30ce8cbdde7a3fa
Ananya419 <shahiananya381@gmail.com>
2026-03-07 23:17:31 +0530

    LeetCode Daily Challenges solution_35

---
+++ Daily_Challenges_LC/Daily_Challenges_35.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minFlips(self, s: str) -> int:
+        n = len(s)
+        s2 = s + s
+
+        # Build alternating patterns
+        alt1 = ''.join('01'[i % 2] for i in range(2 * n))
+        alt2 = ''.join('10'[i % 2] for i in range(2 * n))
+
+        res = n
+        diff1 = diff2 = 0
+
+        # Sliding window
+        for i in range(2 * n):
+            if s2[i] != alt1[i]:
+                diff1 += 1
+            if s2[i] != alt2[i]:
+                diff2 += 1
+
+            # Shrink window when size > n
+            if i >= n:
+                if s2[i - n] != alt1[i - n]:
+                    diff1 -= 1
+                if s2[i - n] != alt2[i - n]:
+                    diff2 -= 1
+
+            if i >= n - 1:
+                res = min(res, diff1, diff2)
+
+        return res
+    
+# Example Usage:
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.minFlips("111000"))  # Output: 1
+    print(solution.minFlips("010"))     # Output: 0
+    print(solution.minFlips("1110"))    # Output: 1
